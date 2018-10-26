d0df92dda588cf49779b86774f1a40ee7445e4c0
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2018-10-26 19:14:42 +0200

    switching WASDa and mouse movement on G key

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -9,8 +9,9 @@ public class PlayerMovement : MonoBehaviour
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
-    //TODO fix issue with click to move and WSAD conflicting and increasing speed
+
+    bool isInDirectMode = false;//TODO consider making static later
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -21,13 +22,39 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.G))    //TODO G dor gamepad. allow player to map later
+        {
+            isInDirectMode = !isInDirectMode; //toggle mode
+        }
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+    }
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+
+    }
+    private void ProcessMouseMovement()
+    {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit layer: " + cameraRaycaster.layerHit);
             switch (cameraRaycaster.layerHit)
             {
                 case Layer.Walkable:
-                    currentClickTarget = cameraRaycaster.hit.point; 
+                    currentClickTarget = cameraRaycaster.hit.point;
                     break;
 
                 case Layer.Enemy:
@@ -38,7 +65,7 @@ public class PlayerMovement : MonoBehaviour
                     return;
             }
         }
-        var playerToClickPoint = currentClickTarget- transform.position;
+        var playerToClickPoint = currentClickTarget - transform.position;
         if (playerToClickPoint.magnitude >= walkMoveStopRadius)
         {
             m_Character.Move(playerToClickPoint, false, false);
